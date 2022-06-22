3157ad144effa417e308fe5713fc81a020fd10f9
l1nlsp <l1nlsp@protonmail.com>
2022-06-22 08:43:01 +0200

    navbar now in /app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hyperlink } from './Hyperlink';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alpha_club';
+
+  hyperlinks: Hyperlink[] = [
+    { name: 'Home', route: '/' },
+    {
+      name: 'online buchen',
+      dropdown: {
+        value: true,
+        hyperlinks: [
+          {
+            name: 'Ballsport',
+            route: 'https://www.eversports.de/widget/w/qif2oo',
+          },
+          {
+            name: 'Kurse',
+            route:
+              'https://www.mysports.com/studio/YWxwaGFjbHVibmV1ZmFocm46MTIxMDAxMDA3MA%3D%3D/course',
+          },
+          {
+            name: 'Probetraining',
+            route:
+              'https://www.mysports.com/studio/YWxwaGFjbHVibmV1ZmFocm46MTIxMDAxMDA3MA%3D%3D/trial-session/2',
+          },
+          {
+            name: 'Mitgliedschaft',
+            route:
+              'https://www.mysports.com/studio/YWxwaGFjbHVibmV1ZmFocm46MTIxMDAxMDA3MA%3D%3D/contract',
+          },
+        ],
+      },
+    },
+    { name: 'studio', route: '/fitness' },
+    { name: 'Kurse', route: '/kursangebot' },
+    { name: 'Racket', route: '/racket' },
+    { name: 'Mitgliedschaft & Preise', route: '/mitgliedschaft-preise' },
+    { name: 'Jobs', route: '/jobs' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //Angular Material
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { CdkMenuModule } from '@angular/cdk/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationBarComponent],
+  declarations: [AppComponent, LandingComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    CdkMenuModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
